0a856d945f9a067b8be457c3c3f5e2b7a5c871c9
Ishwar Mundhe <ishwarmundhe007@gmail.com>
2025-07-28 14:28:12 +0530

    arrays

---
+++ Module1/Arrays/RemoveDuplicate.js
@@ -0,0 +1,24 @@
+// //
+
+function removeDuplicates(nums) {
+  let x = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > nums[x]) {
+      x = x + 1;
+      nums[x] = nums[i];
+    }
+  }
+  console.log(x + 1);
+  console.log(nums);
+
+  return x + 1;
+}
+
+let nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
+removeDuplicates(nums);
+
+// if ([] == true) {
+//   console.log("1");
+// } else {
+//   console.log("2");
+// }
